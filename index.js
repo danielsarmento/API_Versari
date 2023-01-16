ee1cd133fe108b3a8298e4f0355448f446beadd2
Daniel Sarmento <danielsarmento@users.noreply.github.com>
2023-01-16 16:36:10 -0300

    ajuste

--- index.js
+++ index.js
@@ -9,13 +9,6 @@ const PORT = process.env.PORT;
 const connectDB = require("./src/database/database");
 connectDB();
 
-app.set("view engine", "ejs");
-<<<<<<< HEAD
-=======
-
->>>>>>> 42fa72101a0ed0464694fbf63843891551e7bb96
-app.use(express.static(path.join(__dirname, "public")));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
