8d9149f07533ce6c7bbf4553ee96fed9f0a103d9
Daniel Sarmento <danielsarmento@users.noreply.github.com>
2023-01-26 16:02:23 -0300

    habilitando cors

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const app = express();
+const cors = require('cors');
 
 const routes = require("./src/routes/routes");
 const PORT = process.env.PORT;
 
 const connectDB = require("./src/database/database");
 connectDB();
+
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
